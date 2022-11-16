9cb50b6cf8489c648b93908015a5fee0ebced2fc
mpellino <marcopellino00@gmail.com>
2022-11-16 21:55:19 +0100

    Add model form for Bid.

--- auctions/forms.py
+++ auctions/forms.py
@@ -3,9 +3,17 @@ from django import forms
 from django.forms import models
 
 from .models import Auction
+from .models import Bid
 
 # model for for Auction
 class AuctionForm(forms.ModelForm):
     class Meta:
         model = Auction
         fields = ['title','description','category','initial_price', 'image']
+
+
+# model form for Bid.
+class BidForm(forms.ModelForm):
+    class Meta:
+        model = Bid
+        fields = ['value']
